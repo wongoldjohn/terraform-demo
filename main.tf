4e13395ac7a990845140da318fbbc7618aefaa28
wongoldjohn <wongoldjohn@yahoo.com>
2025-08-25 13:59:07 -0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,78 @@
+# VPC
+resource "aws_vpc" "demo_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = { Name = "demo-vpc" }
+}
+
+# Subnet
+resource "aws_subnet" "demo_subnet" {
+  vpc_id            = aws_vpc.demo_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-2a"
+  tags = { Name = "demo-subnet" }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "demo_igw" {
+  vpc_id = aws_vpc.demo_vpc.id
+  tags = { Name = "demo-igw" }
+}
+
+# Route Table
+resource "aws_route_table" "demo_route_table" {
+  vpc_id = aws_vpc.demo_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.demo_igw.id
+  }
+  tags = { Name = "demo-route-table" }
+}
+
+resource "aws_route_table_association" "demo_rta" {
+  subnet_id      = aws_subnet.demo_subnet.id
+  route_table_id = aws_route_table.demo_route_table.id
+}
+
+# Security Group
+resource "aws_security_group" "demo_sg" {
+  vpc_id = aws_vpc.demo_vpc.id
+  name   = "demo-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+    ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "demo_ec2" {
+  ami           = "ami-0b016c703b95ecbe4" # Amazon Linux 2 in us-east-2
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.demo_subnet.id
+  vpc_security_group_ids = [aws_security_group.demo_sg.id]
+
+  tags = { Name = "demo-ec2" }
+}
